cb867ae3e9e68d14efd0f3ad75ef57f0356f19e6
Ruth Gough <ruthegough5@gmail.com>
2021-09-21 23:53:08 -0500

    JavaScript Variables Exercise: questions 1-9.

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Commenting out my answers below so as not to affect the calculations per each question.*/
+//1. const are variables with values that do not change.
+//2.
+//let TwoDimes=20;
+//console.log(TwoDimes); 
+//3.
+//TwoDimes-=5;
+//console.log(TwoDimes);
+//4.
+//TwoDimes+=7;
+//console.log(TwoDimes);
+//5.
+//TwoDimes--;
+//console.log(TwoDimes);
+//6.
+//var TwoDimes = 20;
+//console.log(20);
+//TwoDimes = "This is not a number";
+//"This is not a number";
+//console.log(TwoDimes);
+//7.
+let blank = ""
+//8.
+console.log(blank);
+//9.???...Unable to get to bonus this time
+typeof undefined===null;
+console.log(blank);
